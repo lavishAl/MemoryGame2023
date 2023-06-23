c29b75964632c67d74924c4310009870da52061b
Alex Taylor <alextaylor@Fannys-MacBook-Pro-2.local>
2023-06-23 13:00:28 -0700

    Added idex and css

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# MemoryGame2023
+# Alex Taylor SEI-Project 1 MemoryGame2023
+
+
+## Game Description - Save Gotham
+"In this memory card game, you take on the role of Batman, the Dark Knight, and your mission is to save Gotham City from its notorious villains. The city is infested with criminals who must be captured and brought to justice. To achieve this, you must find matching pairs of cards that represent these villains.
+
+The game board consists of a grid of cards, each hiding the image of a villain. Your task is to flip over two cards at a time, trying to find a matching pair. If you successfully match a pair, the cards remain face up, representing the captured villains. However, if the two cards do not match, they will be flipped back over, and you must try again.
+
+Beware, time is of the essence! You have a limited number of moves to capture all the villains before they escape and Gotham City falls into chaos. Keep an eye on the number of moves remaining and strategize your choices wisely.
+
+
+## Game pictures
+
+
+## Technologies Used
+This memory Batman game utilizes a combination of JavaScript, HTML, and CSS to create an interactive and immersive gaming experience. JavaScript is employed for various functionalities, such as handling card selection, checking for matches, updating the game state, and managing the win or lose conditions. DOM manipulation is a key aspect of JavaScript usage, allowing for dynamic updates to the game board, counter, and modal windows.
+
+HTML provides the structure and layout of the game, defining the elements, containers, and audio tags required. CSS is responsible for the visual styling, including card design, backgrounds, fonts, and modal windows. By leveraging these technologies, the game achieves seamless interactivity, responsive design, and a captivating user interface, enhancing the overall gameplay and immersing players in the world of Batman as they strive to save Gotham City from its villains.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ img.js
@@ -0,0 +1,56 @@
+// const CARDS = [
+//     {
+//       id: 1,
+//       name: 'Joker',
+//       img: 'https://toppng.com/uploads/preview/joker-guason-freetoedit-joker-cartoon-gif-11563303904rbkh8jknfs.png'
+//     },
+//     {
+//       id: 2,
+//       name: 'Harley Quinn',
+//       img: 'https://p7.hiclipart.com/preview/586/105/229/harley-quinn-joker-batman-starfire-the-new-52-harley-quinn.jpg'
+//     },
+//     {
+//       id: 3,
+//       name: 'Bane',
+//       img: 'https://i.pinimg.com/736x/eb/cf/9c/ebcf9cb9d95878b46cde11646c544aaa--crane-batman-batman-dark-knight.jpg'
+//     },
+//     {
+//       id: 4,
+//       name: 'ClayFace',
+//       img: 'https://p7.hiclipart.com/preview/668/32/863/clayface-batman-roland-daggett-killer-croc-dc-animated-universe-nightwing.jpg'
+//     },
+//     {
+//       id: 5,
+//       name: 'Mr.Freeze',
+//       img: 'https://w7.pngwing.com/pngs/715/618/png-transparent-mr-freeze-batman-nora-fries-animated-series-villain-teen-titans-heroes-superhero-fictional-character.png'
+//     },
+//     {
+//       id: 6,
+//       name: 'Peguin',
+//       img: 'https://img.favpng.com/14/7/14/penguin-batman-comic-book-superhero-dc-animated-universe-png-favpng-10pQ3XNtcqx2kKnNu8whqbUNF.jpg'
+//     },
+//     {
+//       id: 7,
+//       name: 'Scarecrow',
+//       img: 'https://img1.pnghut.com/14/14/8/Muv3rV1KcJ/batman-the-animated-series-costume-design-mythical-creature-dark-knight-outerwear.jpg'
+//     },
+//     {
+//       id: 8,
+//       name: "Ra's al ghul",
+//       img: 'https://img.favpng.com/25/18/23/ra-s-al-ghul-batman-talia-al-ghul-poison-ivy-joker-png-favpng-JXsVb48AJcMR5wkqDyEzgAnNh.jpg'
+//     },
+//     {
+//       id: 9,
+//       name: 'Two-Face',
+//       img:  'https://w7.pngwing.com/pngs/845/930/png-transparent-two-face-batman-joker-animated-series-cartoon-the-villain-television-white-heroes.png'
+//     },
+//     {
+//       id: 10,
+//       name: 'Mad Hatter',
+//       img:
+//         'https://i.pinimg.com/originals/be/8d/bc/be8dbc69a19abbffeb26f4acf5313e7c.png'
+//     }
+   
+//   ];
+
+//   export { CARDS };
